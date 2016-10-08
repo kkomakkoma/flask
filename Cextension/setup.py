7d8bd14914ef837f0f1b230949274f0b72961d67
Jongwon <lastone9182@gmail.com>
2016-10-09 02:34:15 +0900

    Update setup.py
    gcc g++ compile

--- Cextension/setup.py
+++ Cextension/setup.py
@@ -1,10 +1,22 @@
 from distutils.core import setup, Extension
+import subprocess
+import os
+
+os.environ["CC"]="gcc"
+os.environ["CXX"]="g++"
 
 detectionC_mod = Extension('detectionC',
-        sources = ['detectionC.cpp'])
+                           sources = ['detectionC.cpp'],
+                           include_dirs = ['/usr/local/include'],
+                           library_dirs = ['/usr/local/lib'])
+
+detectionC_mod.extra_compile_args = ['-ggdb',
+                                     subprocess.check_output(['pkg-config','--cflags','opencv']),
+                                     subprocess.check_output(['pkg-config','--libs','opencv']),
+                                    ];
 
 setup(name = "detectionC",
-        version = "1.0",
-        description = "OpenCV Video Face Detection Module",
-        ext_modules = [detectionC_mod],
+      version = "1.0",
+      description = "OpenCV Video Face Detection Module",
+      ext_modules = [detectionC_mod],
 )
